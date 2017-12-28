77c386989c5cbed748617e6cd33c52668eec102f
daiyangguang <daiyangguang7@163.com>
2017-12-28 23:01:12 +0800

    add cookie

--- src/test/java/DaDa/ClauseTest/AdminLogin.java
+++ src/test/java/DaDa/ClauseTest/AdminLogin.java
@@ -3,17 +3,40 @@ package DaDa.ClauseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.BasicClientCookie;
 public class AdminLogin {
 	WebDriver driver;
+	int verify;
 	public void initDriver() {
-		System.setProperty("webdriver.chrome.driver","/Users/suisuiyi/Downloads/chromedriver");
+		//mac版本环境变量配置
+//		System.setProperty("webdriver.chrome.driver","/Users/suisuiyi/Downloads/chromedriver");
+		//windows环境变量配置
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://test4-admin.dadaabc.us/welcome/login");
+		driver.get("https://test6-admin.dadaabc.us");
 		driver.manage().window().maximize();
 	}
+	public void plantCookies() {
+		
+	}
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws ClientProtocolException, IOException {
 		AdminLogin login = new AdminLogin();
 		login.initDriver();
+		
 	}
+
 }
